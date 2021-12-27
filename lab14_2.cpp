a4846eabf1724d6af038a6e9f65211c6e5ddedd9
mm <nathaphong_p@cmu.ac.th>
2021-12-27 22:26:04 +0700

    วาดรูป*โดยการป้อนค่า

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,52 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+void showImage(const bool image[][M]){
+    for (int i = 0; i < 72; i++)
+    {
+        cout << "-";
+    }
+    cout << "\n";
+
+    for (int j = 0; j < 30; j++)
+    {
+        cout << "|";
+        for (int k = 0; k < 70; k++)
+        {
+            if (image[j][k] == 0)
+            {
+                cout << " ";
+            }
+            else {
+                cout << "*";
+            }
+            
+        }
+        cout <<"|\n";
+    }
+    
+    for (int i = 0; i < 72; i++)
+    {
+        cout << "-";
+    }
+    cout << "\n";
+
+}
+
+void updateImage(bool image[][M], int a, int b, int c){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if(sqrt(pow(i-b,2)+(pow(j-c,2))) <= a-1)
+            {
+                image[i][j] = 1;
+            }
+            
+        }
+        
+    }
+    
+}
